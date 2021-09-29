9b218846b8cc9c697b61eccc33a14d58bd24bb02
George Lesica <george@lesica.com>
2021-09-28 22:32:16 -0600

    Spruce up wrapper

--- harness/the_count/wrapper.sh
+++ harness/the_count/wrapper.sh
@@ -1,12 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+set -e
+
 if [ $# != 2 ]
 then
   echo "usage: wrapper.sh <needle.fa> <haystack.fa>"
   exit
 fi
-# todo: should this script build the image?
-#
+
 OUTPUT="test.fa"
+
 the_count "$2" "$1" $OUTPUT
+
 # do something with the $OUTPUT file to convert
 # it to do correct format for downstream analysis
+
