1ccb29a9477b2eb892b362f0bd5b796b77272ea6
George Lesica <george@lesica.com>
2021-10-01 12:43:04 -0600

    Resolve weird symlink issues for docker

--- harness/mock-search/prepare_data.py
+++ harness/mock-search/prepare_data.py
@@ -31,4 +31,16 @@ with open(DNA_PATH, 'r') as dna_file:
 
 # We don't need to manipulate the target
 # input data so just bundle it up.
-run(['tar', 'czf', 'shared.tar.gz', TARGET_PATH], capture_output=True)
+# TODO: Make this less stupid
+# The problem is that TARGET_PATH is actually
+# a symlink, so we need the actual file in the
+# tarball, not the symlink. In real life, this
+# script would probably need to read the file
+# and do something with it, so this wouldn't be
+# a problem.
+with open(TARGET_PATH, 'r') as target_file:
+    with open('shared.fa', 'w') as dup_file:
+        for line in target_file:
+            dup_file.write(line)
+
+run(['tar', 'czf', 'shared.tar.gz', 'shared.fa'], capture_output=True)
--- harness/mock-search/run_tool.sh
+++ harness/mock-search/run_tool.sh
@@ -8,4 +8,4 @@ SPREAD_PATH="$2"
 tar xf "$SHARED_PATH"
 tar xf "$SPREAD_PATH"
 
-tool.py targets.fa query.fa
+tool.py shared.fa query.fa
