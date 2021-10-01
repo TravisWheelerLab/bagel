073095aee2f1061eacdac110a931ea104330f2cf
George Lesica <george@lesica.com>
2021-10-01 10:35:37 -0600

    Update scripts for aws

--- harness/transmark-run.sh
+++ harness/transmark-run.sh
@@ -4,7 +4,9 @@ set -e
 
 PROFILE="${1:-docker}"
 WORKDIR="${2:-work}"
-DATASET="${3:-$PWD/fixtures}"
+QUEUE="${3:-nextflow-queue}"
+CONTAINER="${4:-traviswheelerlab/psss-harness}"
+DATASET="${5:-$PWD/fixtures}"
 
 nextflow \
     -C transmark-nextflow.config \
@@ -12,5 +14,7 @@ nextflow \
     --dna_file="$DATASET/queries_dna.sto" \
     --protein_file="$DATASET/queries_protein.sto" \
     --target_file="$DATASET/targets.fa" \
+    -process.queue="$QUEUE" \
+    -process.container="$CONTAINER" \
     -profile "$PROFILE" \
     -work-dir "$WORKDIR"
