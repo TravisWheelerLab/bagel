6a4d4b5895bdc202db34bcb7c8bae426ad339bc7
Travis Wheeler <travis@traviswheeler.com>
2021-10-02 00:15:14 -0600

    Add optional 'docker run --pull' config
    In the case that a docker image on cluster compute nodes is
    out of date, I've added a (commented) config setting that can
    force an image update on all compute nodes. Only works on
    docker 20.10.0 or higher
    
    Also refactored a coulpe small naming issues.

--- harness/transmark-run.sh
+++ harness/transmark-run.sh
@@ -4,7 +4,7 @@ set -e
 
 # TODO: write usage instructions
 
-PROFILE="${1:-gscc}"
+PROFILE="${1:-slurm}"
 WORKDIR="${2:-work}"
 QUEUE="${3:-nextflow-queue}"
 CONTAINER="${4:-traviswheelerlab/psss-harness:latest}"
